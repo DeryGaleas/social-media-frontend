dd5cf98c775edaf6a8e1353d713a80a68c4141ab
Greeko <dery.reyna@gmail.com>
2022-04-06 20:30:31 -0600

    Chat View Advance

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,11 @@
 import { gql, useSubscription } from "@apollo/client";
 import { All_MessagesSubscription } from "../../generated/graphql";
+import { SearchOutlined} from '@ant-design/icons';
+import { Row, Col, Input, Card, Avatar } from 'antd'
+import styled from "styled-components";
+
+
+const { Meta } = Card;
 
 const ALL_MESSAGES = gql`
   subscription all_messages {
@@ -23,7 +29,103 @@ const Dashboard = () => {
       console.log(data);
     },
   });
-  return <h1>Dashboard</h1>;
+  return (
+  <MainContainer>
+    <Row>
+      <Col span={1}></Col>
+      <Col span={9}>
+        <div>
+        <Input placeholder="Buscar" prefix={<SearchOutlined />}/>
+        <ContactContainer >
+
+          <Card>
+            <Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title="Dery Galeas"
+              description="jajajajaja"
+            />
+          </Card>
+
+          <Card>
+            <Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title="Diego Romero"
+              description="Activa el Docker!!!"
+            />
+          </Card>
+
+          <Card>
+            <Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title="Cris"
+              description="Anoun"
+            />
+          </Card>
+
+
+        </ContactContainer>
+        
+        
+        </div>
+
+      </Col>
+      <Col span={1}></Col>
+      <Col span={12}>
+        <ChatContainer>
+        <Card style={{height:""}}>
+            <Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title="Dery Galeas"
+              description="online"
+            />
+          </Card>
+
+          <ChatText>Hola como estas?</ChatText>
+         
+        </ChatContainer>
+
+      </Col>
+      <Col span={1}></Col>
+      
+    </Row>
+  
+    </MainContainer>
+  
+  
+  );
 };
+
+const MainContainer = styled.div`
+  padding-top:30px;
+  
+  
+  
+ 
+
+
+`
+
+const ContactContainer = styled.div`
+  margin-top:20px;
+   
+`
+
+const ChatContainer = styled.div`
+
+  height:500px;
+  background-color:#F4F3EC;
+
+`
+
+const ChatText = styled.div`
+  margin-top:15px;
+  margin-left:30px;
+  background-color: white;
+  width:150px;
+  border: 1px solid #FCFBF7;
+  border-radius: 5px;
+
+`
+
 
 export default Dashboard;
